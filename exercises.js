712d080da36847a0ab0ee532220156230ac9928d
Sean Simpson <JediTiger@users.noreply.github.com>
2026-06-27 00:26:31 -0500

    Exercise 6 completed

--- exercises.js
+++ exercises.js
@@ -22,7 +22,7 @@ if (process.argv[1] === fileURLToPath(import.meta.url)) {
   */
 
   // Modify the line of code BELOW to run a different exercise
-  exercise_05();
+  exercise_06();
   // Modify the line of code ABOVE to run a different exercise
 }
 
@@ -228,26 +228,62 @@ function exercise_06() {
   */
   // CODE IN THE OPEN LINES BELOW
 
-  async function task1(text, callback) {
-    await plannedDelay(1000);
-    callback(text);
-    }
-    async function task2(text, callback) {
-      await plannedDelay(1000);
-      callback(text);
-    }
-    async function task3(text, callback) {
-      await plannedDelay(1000);
-      callback(text);
+  async function task1(text) {
+    ltc("Task 1 function started");
+    return new Promise((resolve,reject) => {
+      ltc("Task 1 promise block started");
+      if (text === "Task 1") {
+        ltc("Task 1 function complete");
+        resolve("Task 1 text checked");
+      } else {
+        ltc("Task 1 function rejected");
+        reject(new Error("Wrong task name passed"));
+      }
+    });
+  }
+  
+  async function task2(text) {
+    return new Promise((resolve,reject) => {
+      ltc("Task 2 promise block started");
+      if (text === "Task 2") {
+        ltc("Task 2 function complete");
+        resolve("Task 2 text checked");
+      } else {
+        ltc("Task 2 function rejected");
+        reject("Wrong task name passed");
+      }
+    });
+  }
 
-    }
-  task1("Task 1", (result) => { ltc(result) 
-    task2("Task 2", (result) => { ltc(result) 
-      task3("Task 3", (result) => { ltc(result) 
-        });
-      });
-  });
+  async function task3(text) {
+    return new Promise((resolve,reject) => {
+      ltc("Task 3 promise block started");
+      if (text === "Task 3") {
+        ltc("Task 3 function complete");
+        resolve("Task 3 text checked");
+      } else {
+        ltc("Task 3 function rejected");
+        reject("Wrong task name passed");
+      }
+    });
+  }
 
+task1("Task 1")
+.then(result => {
+  ltc(result);
+  ltc("Task 1 executed");
+  return task2("Task 2");
+})
+.then(result => {
+  ltc(result);
+  ltc("Task 2 executed");
+  return task3("Task 3");
+})
+.then(result => {
+  ltc(result);
+  ltc("Task 3 executed");
+})
+.catch((error) => {});
   // CODE IN THE OPEN LINES ABOVE
 }
 
